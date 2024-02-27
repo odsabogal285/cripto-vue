0fd53083b72aa136db0de47570d9df22ab582a1d
Oscar Sabogal <oscarsabogal285@gmail.com>
2024-02-27 18:51:30 -0500

    feat: moved to composable useCripto

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,16 @@
 <script setup>
-  import {ref, onMounted, reactive, computed} from "vue";
+  import {ref, reactive} from "vue";
   import Alerta from "./components/Alerta.vue";
   import Spinner from "./components/Spinner.vue";
+  import useCripto from "./composables/useCripto.js";
 
-  const monedas = ref([
-    { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
-    { codigo: 'MXN', texto: 'Peso Mexicano'},
-    { codigo: 'EUR', texto: 'Euro'},
-    { codigo: 'GBP', texto: 'Libra Esterlina'},
-    { codigo: 'COP', texto: 'Peso Colombiano'},
-  ]);
+  const { monedas, criptomonedas, cargando, cotizacion, obtenerCotizacion, mostrarResultado} = useCripto();
 
-  const criptomonedas = ref([]);
   const error = ref('');
 
   const cotizar = reactive({
     moneda: '',
     criptomoneda: ''
-  });
-
-  const cotizacion = ref({});
-  const cargando = ref(false);
-
-  onMounted(() => {
-    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(({Data}) => {
-          criptomonedas.value = Data;
-        })
   });
 
   const cotizarCripto = () => {
@@ -37,29 +19,9 @@
       return
     }
     error.value = '';
-    obtenerCotizacion();
+    obtenerCotizacion(cotizar);
   }
 
-  const obtenerCotizacion = async () => {
-    cargando.value = true;
-    cotizacion.value = {};
-    try {
-      const {moneda, criptomoneda} = cotizar;
-      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
-      const respuesta = await fetch(url);
-      const data = await respuesta.json();
-
-      cotizacion.value = data.DISPLAY[criptomoneda][moneda];
-    } catch (error) {
-      console.log(error)
-    } finally {
-      cargando.value = false;
-    }
-  }
-
-  const mostrarResultado = computed(() => {
-    return Object.values(cotizacion.value).length > 0
-  });
 </script>
 
 <template>
